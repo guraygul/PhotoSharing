827a11fcbbe664c1a044353e213c153dd0fa05c6
Güray Gül <guraygul59@gmail.com>
2024-01-28 17:09:05 +0300

    Finished PhotoSharing app

--- PhotoSharing/FeedViewController.swift
+++ PhotoSharing/FeedViewController.swift
@@ -6,13 +6,78 @@
 //
 
 import UIKit
+import Firebase
+import SDWebImage
 
-class FeedViewController: UIViewController {
-
+class FeedViewController: UIViewController, UITableViewDelegate, UITableViewDataSource {
+    
+    @IBOutlet weak var tableView: UITableView!
+    
+    var postDictionary = [Post]()
+    //    var emailDictionary = [String]()
+    //    var commentDictionary = [String]()
+    //    var imageDictionary = [String]()
+    
     override func viewDidLoad() {
         super.viewDidLoad()
-
-        // Do any additional setup after loading the view.
+        
+        tableView.delegate = self
+        tableView.dataSource = self
+        
+        firebaseGetData()
+    }
+    
+    func firebaseGetData() {
+        let firestoreDatabase = Firestore.firestore()
+        
+        firestoreDatabase.collection("Post").order(by: "date", descending: true).addSnapshotListener { snapshot, error in
+            
+            if error != nil {
+                print(error?.localizedDescription)
+            } else {
+                if snapshot?.isEmpty != true && snapshot != nil {
+                    
+                    //                    self.emailDictionary.removeAll(keepingCapacity: false)
+                    //                    self.imageDictionary.removeAll(keepingCapacity: false)
+                    //                    self.commentDictionary.removeAll(keepingCapacity: false)
+                    self.postDictionary.removeAll(keepingCapacity: false)
+                    
+                    for document in snapshot!.documents {
+                        //let documentId = document.documentID
+                        
+                        if let imageUrl = document.get("imageUrl") as? String {
+                            if let comment = document.get("comment") as? String {
+                                if let email = document.get("email") as? String {
+                                    
+                                    let post = Post(email: email, comment: comment, imageUrl: imageUrl)
+                                    self.postDictionary.append(post)
+                                }
+                            }
+                        }
+                        
+                    }
+                    
+                    self.tableView.reloadData()
+                }
+            }
+        }
+    }
+    
+    func tableView(_ tableView: UITableView, numberOfRowsInSection section: Int) -> Int {
+        return postDictionary.count
+    }
+    func tableView(_ tableView: UITableView, heightForRowAt indexPath: IndexPath) -> CGFloat {
+        return 200
+    }
+    
+    func tableView(_ tableView: UITableView, cellForRowAt indexPath: IndexPath) -> UITableViewCell {
+        
+        let cell = tableView.dequeueReusableCell(withIdentifier: "Cell", for: indexPath) as! FeedTableViewCell
+        cell.emailText.text = postDictionary[indexPath.row].email
+        cell.commentText.text = postDictionary[indexPath.row].comment
+        cell.postImageView.sd_setImage(with: URL(string: self.postDictionary[indexPath.row].imageUrl))
+        
+        return cell
     }
     
 }
--- PhotoSharing/UploadViewController.swift
+++ PhotoSharing/UploadViewController.swift
@@ -6,6 +6,8 @@
 //
 
 import UIKit
+import Firebase
+import FirebaseStorage
 
 class UploadViewController: UIViewController, UIImagePickerControllerDelegate, UINavigationControllerDelegate {
     
@@ -35,6 +37,53 @@ class UploadViewController: UIViewController, UIImagePickerControllerDelegate, U
     }
     
     @IBAction func didUploadTapped(_ sender: Any) {
+        let storage = Storage.storage()
+        let storageReference = storage.reference()
+        let mediaFolder = storageReference.child("media")
+        
+        if let data = imageView.image?.jpegData(compressionQuality: 0.5) {
+            let uuid = UUID().uuidString
+            
+            let imageReference = mediaFolder.child("\(uuid).jpg")
+            
+            imageReference.putData(data, metadata: nil) { (storageMetaData, error) in
+                if error != nil {
+                    self.throwAlert(titleInput: "Error", messageInput: error?.localizedDescription ?? "An error occured")
+                } else {
+                    imageReference.downloadURL { (url, error) in
+                        if error == nil {
+                            let imageUrl = url?.absoluteString
+                            
+                            if let imageUrl = imageUrl {
+                                let firestoreDatabase = Firestore.firestore()
+                                let firestorePost = ["imageUrl" : imageUrl, "comment" : self.commentTextField.text!, "email" : Auth.auth().currentUser!.email!, "date" : FieldValue.serverTimestamp()] as [String : Any]
+                                
+                                firestoreDatabase.collection("Post").addDocument(data: firestorePost) { (error) in
+                                    if error != nil {
+                                        self.throwAlert(titleInput: "Error", messageInput: error?.localizedDescription ?? "An error occured!")
+                                    } else {
+                                        self.imageView.image = UIImage(systemName: "square.and.arrow.up")
+                                        self.commentTextField.text = ""
+                                        self.tabBarController?.selectedIndex = 0
+                                    }
+                                }
+                            }
+                            
+                            
+                            
+                        }
+                    }
+                }
+            }
+        }
+    }
+    
+    func throwAlert(titleInput: String, messageInput: String) {
+        let alert = UIAlertController(title: titleInput, message: messageInput, preferredStyle: .alert)
+        let okButton = UIAlertAction(title: "OK", style: .default, handler: nil)
+        
+        alert.addAction(okButton)
+        self.present(alert, animated: true, completion: nil)
     }
     
 }
